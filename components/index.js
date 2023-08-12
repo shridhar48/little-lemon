d51901b132b538e42b122148be2c8281d9530f4a
shridhar48 <shridharbhat48@gmail.com>
2023-08-12 17:26:37 +0530

    project complete

--- components/index.js
+++
@@ -1,3 +0,0 @@
-import Initials from './Initials';
-
-export { Initials };
--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -1,101 +1,279 @@
-import { View, Text, Image, TextInput, Pressable, Alert } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import React, { useState } from 'react';
-
+import React, { useState, useRef, useContext, useCallback } from "react";
 import {
-  validateName,
-  validateEmail,
-  saveItemToAsyncStorage,
-} from '../shared/utils';
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  Pressable,
+} from "react-native";
+import PagerView from "react-native-pager-view";
+import { validateEmail, validateName } from "../utils";
+import Constants from "expo-constants";
 
-import { CONSTANTS } from '../shared/constants';
-import styles from './styles';
-
-const { SCREENS, ASYNC_STORAGE_CONSTANTS, PROFILE_MODEL } = CONSTANTS;
+import { AuthContext } from "../contexts/AuthContext";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 const Onboarding = () => {
-  const [firstName, setFirstNasme] = useState('');
-  const [email, setEmail] = useState('');
+  const [firstName, onChangeFirstName] = useState("");
+  const [lastName, onChangeLastName] = useState("");
+  const [email, onChangeEmail] = useState("");
 
-  const navigation = useNavigation();
+  const isEmailValid = validateEmail(email);
+  const isFirstNameValid = validateName(firstName);
+  const isLastNameValid = validateName(lastName);
+  const viewPagerRef = useRef(PagerView);
 
-  const validateInputs = async () => {
-    const isValidFirstName = validateName(firstName);
-    const isValidEmail = validateEmail(email);
+  const { onboard } = useContext(AuthContext);
 
-    if (isValidFirstName && isValidEmail) {
-      const profileObj = { ...PROFILE_MODEL };
+  // FONTS
+  const [fontsLoaded] = useFonts({
+    "Karla-Regular": require("../assets/fonts/Karla-Regular.ttf"),
+    "Karla-Medium": require("../assets/fonts/Karla-Medium.ttf"),
+    "Karla-Bold": require("../assets/fonts/Karla-Bold.ttf"),
+    "Karla-ExtraBold": require("../assets/fonts/Karla-ExtraBold.ttf"),
+    "MarkaziText-Regular": require("../assets/fonts/MarkaziText-Regular.ttf"),
+    "MarkaziText-Medium": require("../assets/fonts/MarkaziText-Medium.ttf"),
+  });
 
-      console.log('Valid inputs');
-
-      profileObj.firstName = firstName;
-      profileObj.email = email;
-
-      await saveItemToAsyncStorage(
-        ASYNC_STORAGE_CONSTANTS.PROFILE,
-        JSON.stringify(profileObj)
-      );
-      await saveItemToAsyncStorage(ASYNC_STORAGE_CONSTANTS.LOGGED_IN, 'true');
-
-      navigation.navigate(SCREENS.PROFILE);
-    } else {
-      console.log('Invalid inputs');
-
-      Alert.alert('Please enter correct firstname and email');
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
     }
-  };
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      onLayout={onLayoutRootView}
+    >
       <View style={styles.header}>
-        <Image source={require('../assets/images/Logo.png')} />
+        <Image
+          style={styles.logo}
+          source={require("../img/littleLemonLogo.png")}
+          accessible={true}
+          accessibilityLabel={"Little Lemon Logo"}
+        />
       </View>
-      <View style={styles.body}>
-        <Text style={styles.mainText}>Let us get to know you</Text>
-        <View style={styles.bodyContainer}>
-          <View style={styles.textInputViewContainer}>
-            <Text style={styles.labelText}>First Name</Text>
+      <Text style={styles.welcomeText}>Let us get to know you</Text>
+      <PagerView
+        style={styles.viewPager}
+        scrollEnabled={false}
+        initialPage={0}
+        ref={viewPagerRef}
+      >
+        <View style={styles.page} key="1">
+          <View style={styles.pageContainer}>
+            <Text style={styles.text}>First Name</Text>
             <TextInput
-              editable
-              multiline
-              maxLength={40}
-              onChangeText={(text) => {
-                setFirstNasme(text);
-              }}
+              style={styles.inputBox}
               value={firstName}
-              style={styles.textInputContainer}
-              cursorColor={'black'}
+              onChangeText={onChangeFirstName}
+              placeholder={"First Name"}
             />
           </View>
-          <View style={styles.textInputViewContainer}>
-            <Text style={styles.labelText}>Email</Text>
+          <View style={styles.pageIndicator}>
+            <View style={[styles.pageDot, styles.pageDotActive]}></View>
+            <View style={styles.pageDot}></View>
+            <View style={styles.pageDot}></View>
+          </View>
+          <Pressable
+            style={[styles.btn, isFirstNameValid ? "" : styles.btnDisabled]}
+            onPress={() => viewPagerRef.current.setPage(1)}
+            disabled={!isFirstNameValid}
+          >
+            <Text style={styles.btntext}>Next</Text>
+          </Pressable>
+        </View>
+        <View style={styles.page} key="2">
+          <View style={styles.pageContainer}>
+            <Text style={styles.text}>Last Name</Text>
             <TextInput
-              editable
-              multiline
-              maxLength={40}
-              onChangeText={(text) => {
-                setEmail(text);
-              }}
-              keyboardType='email-address'
-              value={email}
-              style={styles.textInputContainer}
-              cursorColor={'black'}
+              style={styles.inputBox}
+              value={lastName}
+              onChangeText={onChangeLastName}
+              placeholder={"Last Name"}
             />
+          </View>
+          <View style={styles.pageIndicator}>
+            <View style={styles.pageDot}></View>
+            <View style={[styles.pageDot, styles.pageDotActive]}></View>
+            <View style={styles.pageDot}></View>
+          </View>
+          <View style={styles.buttons}>
+            <Pressable
+              style={styles.halfBtn}
+              onPress={() => viewPagerRef.current.setPage(0)}
+            >
+              <Text style={styles.btntext}>Back</Text>
+            </Pressable>
+            <Pressable
+              style={[
+                styles.halfBtn,
+                isLastNameValid ? "" : styles.btnDisabled,
+              ]}
+              onPress={() => viewPagerRef.current.setPage(2)}
+              disabled={!isLastNameValid}
+            >
+              <Text style={styles.btntext}>Next</Text>
+            </Pressable>
           </View>
         </View>
-      </View>
-      <View style={styles.footer}>
-        <Pressable
-          style={styles.buttonContainer}
-          onPress={() => {
-            validateInputs();
-          }}
-        >
-          <Text style={styles.mainText}>Next</Text>
-        </Pressable>
-      </View>
-    </View>
+        <View style={styles.page} key="3">
+          <View style={styles.pageContainer}>
+            <Text style={styles.text}>Email</Text>
+            <TextInput
+              style={styles.inputBox}
+              value={email}
+              onChangeText={onChangeEmail}
+              placeholder={"Email"}
+              keyboardType="email-address"
+            />
+          </View>
+          <View style={styles.pageIndicator}>
+            <View style={styles.pageDot}></View>
+            <View style={styles.pageDot}></View>
+            <View style={[styles.pageDot, styles.pageDotActive]}></View>
+          </View>
+          <View style={styles.buttons}>
+            <Pressable
+              style={styles.halfBtn}
+              onPress={() => viewPagerRef.current.setPage(1)}
+            >
+              <Text style={styles.btntext}>Back</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.halfBtn, isEmailValid ? "" : styles.btnDisabled]}
+              onPress={() => onboard({ firstName, lastName, email })}
+              disabled={!isEmailValid}
+            >
+              <Text style={styles.btntext}>Submit</Text>
+            </Pressable>
+          </View>
+        </View>
+      </PagerView>
+    </KeyboardAvoidingView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Constants.statusBarHeight,
+  },
+  header: {
+    padding: 12,
+    flexDirection: "row",
+    justifyContent: "center",
+    backgroundColor: "#dee3e9",
+  },
+  logo: {
+    height: 50,
+    width: 150,
+    resizeMode: "contain",
+  },
+  viewPager: {
+    flex: 1,
+  },
+  page: {
+    justifyContent: "center",
+  },
+  pageContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  welcomeText: {
+    fontSize: 40,
+    paddingVertical: 60,
+    fontFamily: "MarkaziText-Medium",
+    color: "#495E57",
+    textAlign: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontFamily: "Karla-ExtraBold",
+    color: "#495E57",
+  },
+  inputBox: {
+    borderColor: "#EDEFEE",
+    backgroundColor: "#EDEFEE",
+    alignSelf: "stretch",
+    height: 50,
+    margin: 18,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 20,
+    borderRadius: 9,
+    fontFamily: "Karla-Medium",
+  },
+  btn: {
+    backgroundColor: "#f4ce14",
+    borderColor: "#f4ce14",
+    borderRadius: 9,
+    alignSelf: "stretch",
+    marginHorizontal: 18,
+    marginBottom: 60,
+    padding: 10,
+    borderWidth: 1,
+  },
+  btnDisabled: {
+    backgroundColor: "#f1f4f7",
+  },
+  buttons: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginLeft: 18,
+    marginBottom: 60,
+  },
+  halfBtn: {
+    flex: 1,
+    borderColor: "#f4ce14",
+    backgroundColor: "#f4ce14",
+    borderRadius: 9,
+    alignSelf: "stretch",
+    marginRight: 18,
+    padding: 10,
+    borderWidth: 1,
+  },
+  btntext: {
+    fontSize: 22,
+    color: "#333",
+    fontFamily: "Karla-Bold",
+    alignSelf: "center",
+  },
+  pageIndicator: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+  pageDot: {
+    backgroundColor: "#67788a",
+    width: 22,
+    height: 22,
+    marginHorizontal: 10,
+    borderRadius: 11,
+  },
+  pageDotActive: {
+    backgroundColor: "#f4ce14",
+    width: 22,
+    height: 22,
+    borderRadius: 11,
+  },
+});
 
 export default Onboarding;
